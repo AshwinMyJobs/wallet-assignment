3b3b9160c4ee7207e0958867604305af1046853d
AshwinMyJobs <myjobsash@gmail.com>
2025-12-16 22:51:33 +0530

    Initial code commit

---
+++ wallet-assignement/src/main/java/com/wallet/WalletAssignementApplication.java
@@ -0,0 +1,13 @@
+package com.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletAssignementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletAssignementApplication.class, args);
+	}
+
+}
